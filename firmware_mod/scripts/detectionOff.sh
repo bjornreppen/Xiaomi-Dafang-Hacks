8b5537734162a33715b2bb2654dfc5cd01ce7728
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2018-07-07 16:02:32 +0200

    Common prefix for mqtt motion topic

--- firmware_mod/scripts/detectionOff.sh
+++ firmware_mod/scripts/detectionOff.sh
@@ -12,7 +12,7 @@ fi
 # Publish a mqtt message
 if [ "$publish_mqtt_message" = true ] ; then
 	. /system/sdcard/config/mqtt.conf
-	/system/sdcard/bin/mosquitto_pub.bin -h "$HOST" -p "$PORT" -u "$USER" -P "$PASS" -t "${TOPIC}"/"${HOSTNAME}"/motion ${MOSQUITTOOPTS} ${MOSQUITTOPUBOPTS} -m "OFF"
+	/system/sdcard/bin/mosquitto_pub.bin -h "$HOST" -p "$PORT" -u "$USER" -P "$PASS" -t "${TOPIC}"/motion/"${HOSTNAME}" ${MOSQUITTOOPTS} ${MOSQUITTOPUBOPTS} -m "OFF"
 fi
 
 # Run any user scripts.
